48e46aad6fc292dfe1f112be449d481321944fb2
Gustavo-Kissel <kisselgustavo@gmail.com>
2023-11-14 15:16:44 -0300

    Feature fetch Home products from API

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,6 @@
 import data from '../data.json'
 
-export default async function GET() {
+export async function GET() {
   const featuredProducts = data.products.filter((product) => product.featured)
 
   return Response.json(featuredProducts)
